e4c3b535a3e366b50da103899b37595f333bea51
Michael Halim <halimmichael09@gmail.com>
2020-09-23 19:00:15 +0700

    Initial lab2

---
+++ lab2/src/main/java/id/ac/ui/cs/mobileprogramming/michaelwh/lab2/FirstFragment.kt
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.mobileprogramming.michaelwh.lab2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private var counter: Int = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            counter ++
+            textview_first.text = counter.toString()
+        }
+    }
+}
