d493e32ad6e61ccfcdca34f12cb7bbc4ff239439
Michael Halim <halimmichael09@gmail.com>
2020-11-26 16:15:52 +0700

    Init lab 8

---
+++ lab-8/src/main/java/id/ac/ui/cs/mobileprogramming/michaelwiryadinatahalim/lab_8/MainActivity.kt
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.mobileprogramming.michaelwiryadinatahalim.lab_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ui.cs.mobileprogramming.michaelwiryadinatahalim.lab_8.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
